325b7c6810526f45504598a6db244aa6d7be2111
Victor Assis <victorluisgassis@gmail.com>
2023-11-16 01:11:56 -0300

    Remove unnecessary variables from ec2 module

--- terraform-project/modules/ec2/variables.tf
+++ terraform-project/modules/ec2/variables.tf
@@ -46,28 +46,3 @@ variable "alb_sg_id" {
 variable "PATH_TO_YOUR_PUBLIC_KEY" {
   description = "The path to the public SSH key to be used for EC2 instances"
 }
-
-#variable "database_host" {
-#  description = "The hostname of the database"
-#  type        = string
-#}
-#
-#variable "database_name" {
-#  description = "The name of the database"
-#  type        = string
-#}
-#
-#variable "database_pass" {
-#  description = "The password of the database"
-#  type        = string
-#}
-#
-#variable "database_port" {
-#  description = "The port of the database"
-#  type        = string
-#}
-#
-#variable "database_user" {
-#  description = "The username of the database"
-#  type        = string  
-#}
