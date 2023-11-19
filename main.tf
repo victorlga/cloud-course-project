2edda22d6607bf168848564c97895832eb7f4b2f
Victor Assis <victorluisgassis@gmail.com>
2023-11-19 14:53:07 -0300

    Organize code

--- main.tf
+++ main.tf
@@ -80,27 +80,32 @@ module "sg" {
   vpc_id = module.vpc.vpc_id
 }
 
+module "iam" {
+  source = "./modules/iam"
+}
+
 module "ec2" {
-  source = "./modules/ec2"
+  source                      = "./modules/ec2"
 
-  ami                     = "ami-0fc5d935ebf8bc3bc"
-  instance_type           = "t2.micro"
+  ami                         = "ami-0fc5d935ebf8bc3bc"
+  instance_type               = "t2.micro"
 
-  vpc_id                  = module.vpc.vpc_id
-  public_sub_1_id         = module.vpc.public_sub_1_id
-  public_sub_2_id         = module.vpc.public_sub_2_id
-  private_sub_1_id        = module.vpc.private_sub_1_id
-  private_sub_2_id        = module.vpc.private_sub_2_id
+  vpc_id                      = module.vpc.vpc_id
+  public_sub_1_id             = module.vpc.public_sub_1_id
+  public_sub_2_id             = module.vpc.public_sub_2_id
+  private_sub_1_id            = module.vpc.public_sub_1_id
+  private_sub_2_id            = module.vpc.public_sub_2_id
 
-  ec2_sg_id               = module.sg.ec2_sg_id
-  alb_sg_id               = module.sg.alb_sg_id
+  ec2_sg_id                   = module.sg.ec2_sg_id
+  alb_sg_id                   = module.sg.alb_sg_id
 
-  db_host                 = module.rds.db_host
-  db_name                 = var.db_name
-  db_username             = var.db_username
-  db_password             = var.db_password
+  #db_host                     = module.rds.db_host
+  db_name                     = var.db_name
+  db_username                 = var.db_username
+  db_password                 = var.db_password
 
-  PATH_TO_YOUR_PUBLIC_KEY = "/home/victor/.ssh/id_rsa.pub"
+  ec2_profile_name            = module.iam.ec2_profile_name
+  PATH_TO_YOUR_PUBLIC_KEY     = "/home/victor/.ssh/id_rsa.pub"
 }
 
 module "rds" {
