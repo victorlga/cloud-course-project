54832e6a00d5b12de8f0c3060834bbcbe94b9b9e
Victor Assis <victorluisgassis@gmail.com>
2023-11-19 14:51:34 -0300

    Remove unnecessary comments

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -2,8 +2,3 @@ output "ec2_profile_name" {
   description = "The name of the IAM instance profile for EC2 instances"
   value = aws_iam_instance_profile.ec2_profile.name
 }
-
-# output "rds_role_arn" {
-#     description = "The ARN of the IAM role for RDS instances"
-#     value = aws_iam_role.rds_role.arn
-# }
--- modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-# variable "aws_region" {
-#   description = "AWS region"
-#   type        = string
-# }
-
-# variable "aws_account_id" {
-#   description = "AWS account ID"
-#   type        = string
-# }
